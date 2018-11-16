83a7f16af681b9fe8a86e9a22198024fedfc0c33
Roman Tomjak <6570684+romantomjak@users.noreply.github.com>
2018-11-16 19:51:08 +0000

    remove unnecessary test

--- smtp/server_test.go
+++ smtp/server_test.go
@@ -1,61 +1,32 @@
 package smtp
 
 import (
-	"context"
-	"os"
-	"os/signal"
-	"syscall"
 	"testing"
-	"time"
 )
 
-func waitSig(t *testing.T, c <-chan os.Signal, sig os.Signal) {
-	select {
-	case s := <-c:
-		if s != sig {
-			t.Fatalf("Signal was %v, want %v", s, sig)
-		}
-
-	case <-time.After(1 * time.Second):
-		t.Fatalf("Timeout waiting for %v", sig)
-	}
-}
-
-func Test_Server_Configuration_Defaults(t *testing.T) {
+func newServer() *server {
 	config := Config{
-		Addr: ":0",
+		Addr: "127.0.0.1:0",
 	}
+
 	shutdownChan := make(chan bool)
 
-	smtpServer := NewServer(config, shutdownChan)
-
-	var expected = 0
-	if smtpServer.config.ReadTimeout == 0 {
-		t.Errorf("Expected ReadTimeout to be '%v', but got '%v'", expected, smtpServer.config.ReadTimeout)
-	}
-
-	if smtpServer.config.WriteTimeout == 0 {
-		t.Errorf("Expected WriteTimeout to be '%v', but got '%v'", expected, smtpServer.config.WriteTimeout)
-	}
-
-	if smtpServer.config.nextCommandTimeout == 0 {
-		t.Errorf("Expected nextCommandTimeout to be '%v', but got '%v'", expected, smtpServer.config.nextCommandTimeout)
-	}
+	return NewServer(config, shutdownChan)
 }
 
-func Test_Server_Responds_To_Signals(t *testing.T) {
-	config := Config{
-		Addr: ":0",
+func Test_Default_Server_Timeout_Values_Are_Set(t *testing.T) {
+	expected := 0
+	server := newServer()
+
+	if server.config.ReadTimeout == 0 {
+		t.Errorf("Expected ReadTimeout to be '%v', but got '%v'", expected, server.config.ReadTimeout)
 	}
 
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, syscall.SIGTERM, syscall.SIGINT)
+	if server.config.WriteTimeout == 0 {
+		t.Errorf("Expected WriteTimeout to be '%v', but got '%v'", expected, server.config.WriteTimeout)
+	}
 
-	rootCtx, _ := context.WithCancel(context.Background())
-	shutdownChan := make(chan bool)
-
-	smtpServer := NewServer(config, shutdownChan)
-	go smtpServer.Start(rootCtx)
-
-	waitSig(t, sigChan, syscall.SIGINT)
+	if server.config.nextCommandTimeout == 0 {
+		t.Errorf("Expected nextCommandTimeout to be '%v', but got '%v'", expected, server.config.nextCommandTimeout)
+	}
 }
