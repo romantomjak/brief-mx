65cbd6fa41f29d45df17e8a506208e7eedb46782
Roman Tomjak <6570684+romantomjak@users.noreply.github.com>
2018-11-23 00:30:29 +0000

    add send/recv timeouts and max line length validation

--- smtp/transport.go
+++ smtp/transport.go
@@ -2,30 +2,59 @@ package smtp
 
 import (
 	"bufio"
+	"errors"
 	"net"
 	"strings"
+	"time"
 
 	log "github.com/sirupsen/logrus"
 )
 
 // The underlying transport for Session objects
-type Transport struct {
+type transport struct {
 	conn	net.Conn
-	reader	*bufio.Reader
+
+	timeouts *TransportTimeouts
+
+	reader  *bufio.Reader
+	writer *bufio.Writer
 }
 
-func NewTransport(conn net.Conn) *Transport {
-	return &Transport{
-		conn: conn,
-		reader: bufio.NewReader(conn),
+// Transport layer timeouts
+type TransportTimeouts struct {
+	read  time.Duration // Socket timeout for read operations
+	write time.Duration // Socket timeout for write operations
+}
+
+var DefaultTransportTimeouts = &TransportTimeouts{
+	read:  30 * time.Second,
+	write: 30 * time.Second,
+}
+
+var ErrServerLineTooLong = errors.New("Server line too long. Maxiumum 512b")
+var ErrClientLineTooLong = errors.New("Client line too long. Maxiumum 1000b")
+
+func NewTransport(conn net.Conn, config *TransportTimeouts) *transport {
+	return &transport{
+		conn:     conn,
+		timeouts: config,
+		reader:   bufio.NewReader(conn),
+		writer:   bufio.NewWriter(conn),
 	}
 }
 
 // Reads a line off the wire
-func (t *Transport) ReadLine() (line string, err error) {
+func (t *transport) ReadLine() (line string, err error) {
+	err = t.conn.SetReadDeadline(time.Now().Add(t.timeouts.read))
+	if err != nil {
+		return "", err
+	}
 	line, err = t.reader.ReadString('\n')
 	if err != nil {
 		return "", err
+	}
+	if len(line) > 1000 {
+		return "", ErrClientLineTooLong
 	}
 	trimmedLine := strings.TrimRight(line, "\r\n")
 	log.Debugf("Read: \"%v\"", trimmedLine)
@@ -33,9 +62,20 @@ func (t *Transport) ReadLine() (line string, err error) {
 }
 
 // Sends a line onto the wire
-func (t *Transport) SendLine(line string) (err error) {
-	// FIXME: probably shouldn't ignore this. maybe buffered writer?
-	_, err = t.conn.Write([]byte(line + "\r\n"))
+func (t *transport) SendLine(line string) (err error) {
+	if len(line) > 512 {
+		return ErrServerLineTooLong
+	}
+
+	err = t.conn.SetWriteDeadline(time.Now().Add(t.timeouts.write))
+	if err != nil {
+		return err
+	}
+	_, err = t.writer.Write([]byte(line + "\r\n"))
+	if err != nil {
+		return err
+	}
+	err = t.writer.Flush()
 	if err != nil {
 		return err
 	}
--- smtp/transport_test.go
+++ smtp/transport_test.go
@@ -5,9 +5,9 @@ import (
 	"testing"
 )
 
-func TestCanReadALine(t *testing.T) {
+func TestReadsBytesToString(t *testing.T) {
 	client, server := net.Pipe()
-	transport := NewTransport(server)
+	transport := NewTransport(server, DefaultTransportTimeouts)
 
 	go func() {
 		_, err := client.Write([]byte("HELLO SERVER\r\n"))
@@ -28,9 +28,9 @@ func TestCanReadALine(t *testing.T) {
 	}
 }
 
-func TestCanSendALine(t *testing.T) {
+func TestWritesStringToBytes(t *testing.T) {
 	client, server := net.Pipe()
-	transport := NewTransport(server)
+	transport := NewTransport(server, DefaultTransportTimeouts)
 
 	go func() {
 		err := transport.SendLine("HELLO CLIENT")
@@ -52,5 +52,80 @@ func TestCanSendALine(t *testing.T) {
 	}
 }
 
-// TestWriteTimeout
-// TestReadTimeout
+func TestReadTimeout(t *testing.T) {
+	_, server := net.Pipe()
+
+	config := &TransportTimeouts{
+		read:  1,
+		write: 0,
+	}
+	transport := NewTransport(server, config)
+
+	_, err := transport.ReadLine()
+	if err == nil {
+		t.Errorf("Expected: %v", err)
+	}
+
+	if nerr, ok := err.(net.Error); ok {
+		if !nerr.Timeout() {
+			t.Errorf("err.Timeout() = false, want true")
+		}
+	} else {
+		t.Errorf("got %T, want net.Error", err)
+	}
+}
+
+func TestWriteTimeout(t *testing.T) {
+	_, server := net.Pipe()
+
+	config := &TransportTimeouts{
+		read:  0,
+		write: 1,
+	}
+	transport := NewTransport(server, config)
+
+	err := transport.SendLine("YOLO")
+	if err == nil {
+		t.Errorf("Expected: %v", err)
+	}
+
+	if nerr, ok := err.(net.Error); ok {
+		if !nerr.Timeout() {
+			t.Errorf("err.Timeout() = false, want true")
+		}
+	} else {
+		t.Errorf("got %T, want net.Error", err)
+	}
+}
+
+func TestClientMaximumLineLength(t *testing.T) {
+	client, server := net.Pipe()
+	transport := NewTransport(server, DefaultTransportTimeouts)
+
+	go func() {
+		b := make([]byte, 1001)
+		b[999] = '\r'
+		b[1000] = '\n'
+		_, err := client.Write(b)
+		if err != nil {
+			t.Errorf("Failed to write to a pipe: %v", err)
+		}
+	}()
+
+	_, err := transport.ReadLine()
+	if err != ErrClientLineTooLong {
+		t.Errorf("got %T, want smtp.ErrClientLineTooLong", err)
+	}
+}
+
+func TestServerMaximumLineLength(t *testing.T) {
+	_, server := net.Pipe()
+	transport := NewTransport(server, DefaultTransportTimeouts)
+
+	b := make([]byte, 513)
+	err := transport.SendLine(string(b))
+
+	if err != ErrServerLineTooLong {
+		t.Errorf("got %T, want smtp.ErrServerLineTooLong", err)
+	}
+}
